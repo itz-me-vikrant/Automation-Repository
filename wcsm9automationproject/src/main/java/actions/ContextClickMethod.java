78e1e16c67ca110cef12554e8895c57cef39e796
Vikrant Dharam <vikrantdharam8@gmail.com>
2023-10-25 08:03:47 -0700

    commit

--- wcsm9automationproject/src/main/java/actions/ContextClickMethod.java
+++ wcsm9automationproject/src/main/java/actions/ContextClickMethod.java
@@ -4,22 +4,34 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class ContextClickMethod {
-	//used to perform right click action
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		driver.get("https://demoapps.qspiders.com/");
 		driver.findElement(By.xpath("//section[text()='Button']")).click();
 		driver.findElement(By.xpath("//a[text()='Right Click']")).click();
-		driver.findElement(By.xpath("(//button[text()='Right Click'])[1]"));
-		Actions act = new Actions(driver);
-		act.moveToElement(null);
+		WebElement rightClick1 = driver.findElement(By.xpath("(//button[text()='Right Click'])[1]"));
+		WebElement rightClick2 = driver.findElement(By.xpath("(//button[text()='Right Click'])[2]"));
+		WebElement rightClick3 = driver.findElement(By.xpath("(//button[text()='Right Click'])[3]"));
 		
+		Actions act = new Actions(driver);
+		Thread.sleep(3000);
+
+		act.contextClick(rightClick1).perform();
+		Thread.sleep(3000);
+		
+		act.contextClick(rightClick2).perform();
+		Thread.sleep(3000);
+		
+		act.contextClick(rightClick3).perform();
+			
 	}
 
 }
