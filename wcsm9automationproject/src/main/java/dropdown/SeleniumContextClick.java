5307292ba8a19cbcc5fa730402bc1d0337f76a32
Vikrant Dharam <vikrantdharam8@gmail.com>
2023-10-26 10:40:46 -0700

    commit

--- wcsm9automationproject/src/main/java/dropdown/SeleniumContextClick.java
+++ wcsm9automationproject/src/main/java/dropdown/SeleniumContextClick.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class SeleniumDev {
+public class SeleniumContextClick {
 
 	public static void main(String[] args) throws InterruptedException {
 		WebDriver driver=new ChromeDriver();
